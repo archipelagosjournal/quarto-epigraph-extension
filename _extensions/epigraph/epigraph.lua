3c7febec475f21b7188d700303f64a38f5152af8
Brian Ballsun-Stanton <Denubis@users.noreply.github.com>
2024-01-23 16:48:23 +1100

    Initial commit
    Signed-off-by: Brian Ballsun-Stanton <Denubis@users.noreply.github.com>

---
+++ _extensions/epigraph/epigraph.lua
@@ -0,0 +1,63 @@
+return {
+  quarto.log.output("Loading epigraph extension"),
+
+  --renders an epigraph in html within two nested divs (one for the text, 
+  -- then inner for the citation, each with appropricate css call)
+  -- renders in LaTeX within a blockquote, then citation flushright on next line
+  ['epigraph'] = function(args, kwargs, meta)
+    quarto.log.output("Running epigraph extension", "args", args, "kwargs", kwargs)
+
+    -- Get the text and citation. Fail if text="" not present
+    local text = pandoc.utils.stringify(kwargs["text"])
+    if text == nil then
+      quarto.log.error("epigraph extension requires text argument")
+      return nil
+    end
+   
+    
+    
+    local citation = pandoc.utils.stringify(kwargs["citation"])
+    
+    quarto.log.output("epigraph text = '", text, "' citation = '", citation,"'")
+
+    if quarto.doc.is_format("html") then
+
+      -- see if text has ` \\ ` in it. If so, we need to split each line delinitated by ` \\ ` into it's own line and break on <br/>
+      if string.find(text, " \\ ") then
+        local text_lines = {}
+        -- find ` \\ ` and use it as the split token.
+        for line in string.gmatch(text, "([^\\]+)") do
+          table.insert(text_lines, line)
+        end
+
+        text = table.concat(text_lines, "</br>")
+      end
+
+      local html = '<blockquote class="epigraph"><div class="epigraph-text">' .. text .. '</div>'
+      if citation ~= "" then
+        html = html .. '<div class="epigraph-citation"> – ' .. citation .. '</div>'
+      end
+      html = html .. '</blockquote>'
+      return pandoc.RawInline('html', html)
+    elseif quarto.doc.is_format("latex") then
+
+      -- we need to replace ` \\ ` with `\n\n` in the text
+      text = string.gsub(text, " \\ ", "\n\n")
+
+
+      local latex = '\\begin{quote}' .. text .. "\n\n"
+      if citation ~= "" then
+        latex = latex .. '\\hfill -- ' .. citation .. '\n\n'
+      end
+      latex = latex .. '\\end{quote}'
+      return pandoc.RawInline('latex', latex)
+    else
+      return pandoc.Str("[" .. text .. (citation ~= "" and " -- " .. citation or "") .. "]")
+    end
+
+
+    -- Additional processing here if needed
+
+    -- return pandoc.Str("Hello from Epigraph!")
+  end
+}
